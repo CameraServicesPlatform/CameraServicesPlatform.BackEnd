8a1db37afeca8b495a9cc0b923e99cb2517fe705
AnBinh1703 <dan1314705@gmail.com>
2024-09-23 20:44:38 +0700

    update deloy1

--- CameraServicesPlatform/CameraServicesPlatform.BackEnd.API/Program.cs
+++ CameraServicesPlatform/CameraServicesPlatform.BackEnd.API/Program.cs
@@ -17,7 +17,14 @@ builder.Services.AddCors(p => p.AddPolicy(MyAllowSpecificOrigins, builder =>
     // Other configurations...
 }));
 // Add services to the container.
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080); // HTTP
+    options.ListenAnyIP(8081, listenOptions =>
+    {
+        listenOptions.UseHttps("/https/https-cer.pfx", "binhan");
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

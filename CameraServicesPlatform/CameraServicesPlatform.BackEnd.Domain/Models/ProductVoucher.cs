415f27d08a4b01f98cace3d18eb7389e4d53aad8
AnBinh1703 <dan1314705@gmail.com>
2024-10-06 12:50:00 +0700

    update product vourcehr

--- CameraServicesPlatform/CameraServicesPlatform.BackEnd.Domain/Models/ProductVoucher.cs
+++ CameraServicesPlatform/CameraServicesPlatform.BackEnd.Domain/Models/ProductVoucher.cs
@@ -10,21 +10,13 @@
         {
             [Key]
             public Guid ProductVoucherID { get; set; }
-
-
             public Guid ProductID { get; set; }
-
             [ForeignKey(nameof(ProductID))]
             public Product Product { get; set; }
-
-
             public Guid VourcherID { get; set; }
-
             [ForeignKey(nameof(VourcherID))]
             public Vourcher Vourcher { get; set; }
-
             public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-
             public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
         }
     }

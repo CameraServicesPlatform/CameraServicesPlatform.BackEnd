564edf111666754731268bc95982f137b085b6bc
AnBinh1703 <dan1314705@gmail.com>
2024-10-14 01:33:56 +0700

    fix product

--- CameraServicesPlatform/CameraServicesPlatform.BackEnd.Common/DTO/Response/ProductResponse.cs
+++ CameraServicesPlatform/CameraServicesPlatform.BackEnd.Common/DTO/Response/ProductResponse.cs
@@ -41,8 +41,8 @@ namespace CameraServicesPlatform.BackEnd.Common.DTO.Response
 
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 
- 
-        public List<string>? Image { get; set; } = new List<string>();
- 
+
+        public List<ProductImage> listImage { get; set; } = null!;
+
     }
 }

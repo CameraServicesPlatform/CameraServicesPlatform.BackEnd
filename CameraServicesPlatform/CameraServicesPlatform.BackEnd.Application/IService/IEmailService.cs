fb9ea31e227ba8a79ad024e89cf69aa45b2fa4bb
ThaiDuy29042002 <phamthaiduy963258741@gmail.com>
2024-11-10 01:37:01 +0700

    Duy_contractTemplateController_Update_OrderRent

--- CameraServicesPlatform/CameraServicesPlatform.BackEnd.Application/IService/IEmailService.cs
+++ CameraServicesPlatform/CameraServicesPlatform.BackEnd.Application/IService/IEmailService.cs
@@ -5,5 +5,7 @@ public interface IEmailService
     public void SendEmail(string recipient, string subject, string body);
 
     Task SendEmailAsync(string recipient, string subject, string body);
+    Task SendEmailWithAttachments(string recipient, string subject, string body, List<string> attachmentFilePaths);
+
 
 }

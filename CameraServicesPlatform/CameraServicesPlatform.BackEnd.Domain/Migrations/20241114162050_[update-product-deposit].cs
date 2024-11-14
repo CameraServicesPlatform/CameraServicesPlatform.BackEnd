7fd8c7023b47df7a8a4e7d3437280acf3780734d
ThaiDuy29042002 <phamthaiduy963258741@gmail.com>
2024-11-14 23:52:55 +0700

    Duy_Update_table Product_add_DepositProduct

---
+++ CameraServicesPlatform/CameraServicesPlatform.BackEnd.Domain/Migrations/20241114162050_[update-product-deposit].cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CameraServicesPlatform.BackEnd.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateproductdeposit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "DepositProduct",
+                table: "Products",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepositProduct",
+                table: "Products");
+        }
+    }
+}

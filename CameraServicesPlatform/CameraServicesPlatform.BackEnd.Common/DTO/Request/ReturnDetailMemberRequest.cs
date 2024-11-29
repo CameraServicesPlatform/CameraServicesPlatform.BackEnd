57abbe4f4a6ec618613975ec63bc30822c10a238
ThaiDuy29042002 <phamthaiduy963258741@gmail.com>
2024-11-30 01:36:45 +0700

    Duy_Update_ReturnDetailController

---
+++ CameraServicesPlatform/CameraServicesPlatform.BackEnd.Common/DTO/Request/ReturnDetailMemberRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraServicesPlatform.BackEnd.Common.DTO.Request
+{
+    public class ReturnDetailMemberRequest
+    {
+        public Guid OrderID { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string Condition { get; set; }
+    }
+}
